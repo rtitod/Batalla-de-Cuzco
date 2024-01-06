30b9316b23babe804e6a94c54f2dd92350fe1b03
rtitod <rtitod@unsa.edu.pe>
2024-01-06 11:27:04 -0500

    anadido nuevos sprites

--- level.py
+++ level.py
@@ -9,6 +9,8 @@ from random import choices, randrange
 from npc import *
 from animated_sprite import *
 from player import *
+from weapon import Sling
+
 
 class Level:
     def __init__(self, game):
@@ -68,16 +70,17 @@ class Level:
         self.player_pos = 1.5, 5
         self.health_recovery_delay = 1000
     def override_vars(self):
-        self.mini_map = None
-        self.npc_types = None
-        self.npc_boss_types = None
-        self.quantities = None
-        self.boss_quantities = None
-        self.weapon = None
-        self.boss_min_cols = 0/5
-        self.boss_max_cols = 5/5
-        self.boss_min_rows = 0/5
-        self.boss_max_rows = 5/5
+        self.mini_map = mini_map
+        self.npc_types = [ChancaSubversivo]
+        self.npc_boss_types = [GeneralChanca]
+        self.quantities = [8]
+        self.boss_quantities = [1]
+        self.weapon = Sling(self)
+        self.boss_min_cols = 1 / 9
+        self.boss_max_cols = 4 / 9
+        self.boss_min_rows = 7 / 9
+        self.boss_max_rows = 9 / 9
+
     def new_game(self):
         print("Iniciando un nuevo juego")
         self.override_player_pos()
@@ -114,14 +117,14 @@ class Level:
         self.spawn_npc()
         # sprite map
         add_sprite(AnimatedSprite(self))
-        add_sprite(AnimatedSprite(self, pos=(1.5, 1.5)))
-        add_sprite(AnimatedSprite(self, pos=(1.5, 7.5)))
-        add_sprite(AnimatedSprite(self, pos=(5.5, 3.25)))
-        add_sprite(AnimatedSprite(self, pos=(5.5, 4.75)))
-        add_sprite(AnimatedSprite(self, pos=(7.5, 2.5)))
-        add_sprite(AnimatedSprite(self, pos=(7.5, 5.5)))
-        add_sprite(AnimatedSprite(self, pos=(14.5, 1.5)))
-        add_sprite(AnimatedSprite(self, pos=(14.5, 4.5)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(1.5, 1.5)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(1.5, 7.5)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(5.5, 3.25)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(5.5, 4.75)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(7.5, 2.5)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(7.5, 5.5)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(14.5, 1.5)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(14.5, 4.5)))
         add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'red_light/0.png', pos=(14.5, 5.5)))
         add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'red_light/0.png', pos=(14.5, 7.5)))
         add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'red_light/0.png', pos=(12.5, 7.5)))
@@ -131,10 +134,10 @@ class Level:
         add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'red_light/0.png', pos=(10.5, 20.5)))
         add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'red_light/0.png', pos=(3.5, 14.5)))
         add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'red_light/0.png', pos=(3.5, 18.5)))
-        add_sprite(AnimatedSprite(self, pos=(14.5, 24.5)))
-        add_sprite(AnimatedSprite(self, pos=(14.5, 30.5)))
-        add_sprite(AnimatedSprite(self, pos=(1.5, 30.5)))
-        add_sprite(AnimatedSprite(self, pos=(1.5, 24.5)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(14.5, 24.5)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(14.5, 30.5)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(1.5, 30.5)))
+        add_sprite(AnimatedSprite(self, path=self.anim_sprite_path + 'green_light/0.png', pos=(1.5, 24.5)))
 
         self.map = self.mini_map
         self.ways = [-1, 0], [0, -1], [1, 0], [0, 1], [-1, -1], [1, -1], [1, 1], [-1, 1]
